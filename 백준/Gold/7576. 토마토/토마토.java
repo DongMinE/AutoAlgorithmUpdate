8870b8cab57283ee4596f3e88c3732105e184210
DongMinE <91265819+DongMinE@users.noreply.github.com>
2023-04-12 00:51:40 +0900

    [Gold V] Title: 토마토, Time: 652 ms, Memory: 120372 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int m, n, res, arr[][];
+	static int[] dr = { 0, 1, 0, -1 }, dc = { 1, 0, -1, 0 };
+	// static boolean[][] visited;
+	static Queue<int[]> que = new LinkedList<>();
+	static int max = Integer.MIN_VALUE;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+
+		arr = new int[m][n];
+		// visited = new boolean[m][n];
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				if (arr[i][j] == 1)
+					que.add(new int[] { i, j });
+			}
+		}
+		bfs();
+		System.out.println(max);
+
+	}
+
+	private static void bfs() {
+		while (!que.isEmpty()) {
+			int[] num = que.poll();
+
+			for (int i = 0; i < 4; i++) {
+				int newa = num[0] + dr[i];
+				int newb = num[1] + dc[i];
+
+				if (newa < m && newb < n && newa >= 0 && newb >= 0) {
+					if (arr[newa][newb] == 0) {
+						arr[newa][newb] = arr[num[0]][num[1]] + 1;
+						que.add(new int[] { newa, newb });
+					}
+				}
+			}
+		}
+		if (check())
+			max = -1;
+		else {
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < n; j++) {
+					if (max < arr[i][j]) {
+						max = arr[i][j];
+					}
+				}
+			}
+			max = max-1;
+		}
+	}
+
+	private static boolean check() {
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (arr[i][j] == 0)
+					return true;
+			}
+		}
+		return false;
+	}
+}
