88d03c9499ebe3e2c1e6ab5476993d73224fce79
DongMinE <91265819+DongMinE@users.noreply.github.com>
2023-04-14 02:04:56 +0900

    [Silver I] Title: 오목, Time: 152 ms, Memory: 15956 KB -BaekjoonHub

---
+++ 백준/Silver/2615. 오목/오목.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		int[][] arr = new int[19][19];
+		int[][] dir = { { 0, 1 }, { 1, 0 }, { 1, 1 }, { -1, 1 } };
+
+		for (int i = 0; i < 19; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 19; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+//		for (int[] a : arr) System.out.println(Arrays.toString(a));
+		for (int j = 0; j < 19; j++) {
+			for (int i = 0; i < 19; i++) {
+				if (arr[i][j] == 1 || arr[i][j] == 2) {
+					for (int k = 0; k < 4; k++) {
+						int x = i;
+						int y = j;
+						int cnt = 1;
+						
+						// 오목 완성인지 보기
+						while (true) {
+							x += dir[k][0];
+							y += dir[k][1];
+							if (x < 19 && y < 19 && x >= 0 && y >= 0) {
+								if (arr[x][y] == arr[i][j])
+									cnt++;
+								else break;
+
+							} else break;
+						}
+						x = i;
+						y = j;
+						
+						while (true) {
+							x -= dir[k][0];
+							y -= dir[k][1];
+							if (x < 19 && y < 19 && x >= 0 && y >= 0) {
+								if (arr[x][y] == arr[i][j])
+									cnt++;
+								else break;
+
+							} else break;
+						}
+						if(cnt == 5) {
+							System.out.println(arr[i][j]);
+							System.out.println((i+1) + " " + (j+1));
+							return;
+						}
+					}
+				}
+			}
+		}
+		System.out.println(0);
+	}
+}
