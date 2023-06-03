68ce768a9b255f624de0ef2759333722b6397217
DongMinE <91265819+DongMinE@users.noreply.github.com>
2023-06-04 02:16:28 +0900

    [Gold V] Title: 선발 명단, Time: 180 ms, Memory: 15748 KB -BaekjoonHub

---
+++ 백준/Gold/3980. 선발 명단/선발 명단.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int maxSum;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 0; tc < T; tc++) {
+            maxSum = 0;
+            int[][] pos = new int[11][11];
+            boolean[] check = new boolean[11];
+            StringTokenizer st;
+            for (int i = 0; i < 11; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < 11; j++) {
+                    pos[i][j]  = Integer.parseInt(st.nextToken());
+                }
+            }
+            bfs(pos, 0, check, 0); // 선수 점수, 총합, 자리체크, 선수 번호
+            System.out.println(maxSum);
+        }
+    }
+
+    private static void bfs(int[][] pos, int sum, boolean[] check, int player) {
+        if (player == 11) {
+            maxSum = Math.max(maxSum, sum);
+            return;
+        }
+        for (int i = 0; i < 11; i++) {
+            if(pos[player][i] == 0 || check[i]) continue;
+            check[i] = true;
+            bfs(pos, sum+pos[player][i], check, player+1);
+            check[i] = false;
+        }
+    }
+}
