3b9338f4a459b4384b9a0300709bc1a0c75d3c96
김동민 <anwjr41@naver.com>
2023-12-29 01:21:08 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Stack;
 
 public class Main {
 
@@ -7,24 +8,32 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
-            int a = 0;
-            for (int j = 0; j < str.length(); j++) {
-                if (str.charAt(j) == '(') {
-                    a++;
-                } else if (str.charAt(j) == ')') {
-                    a--;
-                    if (a < 0) {
-                        break;
-                    }
-                }
-            }
-            if (a == 0) {
+            if (isTrue(str)) {
                 System.out.println("YES");
             } else {
                 System.out.println("NO");
             }
+
+
         }
 
+    }
+
+    private static boolean isTrue(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char cha = str.charAt(i);
+            if (cha == '(') {
+                stack.push(cha);
+            } else if (cha == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
     }
 
 }
