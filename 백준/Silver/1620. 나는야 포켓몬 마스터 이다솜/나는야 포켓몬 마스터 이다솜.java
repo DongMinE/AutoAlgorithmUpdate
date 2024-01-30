40fd23c24acb466b3fc8d1cbda454a6b810bad2d
김동민 <anwjr41@naver.com>
2024-01-30 15:55:02 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 1724 ms, Memory: 120468 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+
+        HashMap<Integer, String> map = new HashMap<>();
+        HashMap<String, Integer> StringMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String val = br.readLine();
+            map.put(i + 1, val);
+            StringMap.put(val, i+1);
+        }
+        for (int i = 0; i < m; i++) {
+            String input = br.readLine();
+            if (input.matches("\\d+")) {
+                System.out.println(map.get(Integer.parseInt(input)));
+            } else {
+                System.out.println(StringMap.get((input)));
+            }
+        }
+
+//        for (Integer val : map.keySet()) {
+//            System.out.println("key: " + val + ", " + " val: " + map.get(val));
+//        }
+    }
+}
