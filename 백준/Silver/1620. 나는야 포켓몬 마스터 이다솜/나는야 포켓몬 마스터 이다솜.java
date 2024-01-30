ada94f5ac336f555685e1ccd4712c864e38ff17f
김동민 <anwjr41@naver.com>
2024-01-30 15:57:49 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 1136 ms, Memory: 54620 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -19,11 +19,20 @@ public class Main {
             map.put(i + 1, val);
             StringMap.put(val, i+1);
         }
-        for (int i = 0; i < m; i++) {
+//        for (int i = 0; i < m; i++) {
+//            String input = br.readLine();
+//            if (input.matches("\\d+")) {
+//                System.out.println(map.get(Integer.parseInt(input)));
+//            } else {
+//                System.out.println(StringMap.get((input)));
+//            }
+//        }
+        for(int i = 0; i < m; i++) {
             String input = br.readLine();
-            if (input.matches("\\d+")) {
+            //입력값이 번호인지 포켓몬이름인지 판별
+            if(49 <= input.charAt(0) && input.charAt(0) <= 57) {
                 System.out.println(map.get(Integer.parseInt(input)));
-            } else {
+            }else {
                 System.out.println(StringMap.get((input)));
             }
         }
