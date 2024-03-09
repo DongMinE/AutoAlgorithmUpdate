aa13e8a4815832c00fc22797f4395821fe5aed4e
김동민 <anwjr41@naver.com>
2024-03-10 01:06:16 +0900

    [Silver IV] Title: 좋은 구간, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/1059. 좋은 구간/좋은 구간.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int l = Integer.parseInt(br.readLine());
+        int high = 1001;
+        int row = 0;
+        int res = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < l; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            if (num < n) {
+                if (num > row) {
+                    row = num;
+                }
+            } else if (num > n) {
+                if (num < high) {
+                    high = num;
+                }
+            } else {
+                System.out.println(0);
+                return;
+            }
+        }
+        for (int i = row + 1; i <= n; i++) {
+            for (int j = n; j < high; j++) {
+                if (i != j) {
+                    res++;
+                }
+            }
+        }
+        System.out.println(res);
+    }
+}
