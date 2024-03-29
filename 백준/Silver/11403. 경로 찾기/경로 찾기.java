dfefd80d2e819d40d9285d93e7d4cdb9f6d65df4
김동민 <anwjr41@naver.com>
2024-03-29 16:54:04 +0900

    [Silver I] Title: 경로 찾기, Time: 380 ms, Memory: 19752 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[][] origin;
+    static int[][] resList;
+    static boolean[][] check;
+    static Queue<int[]> q;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        origin = new int[n][n];
+        q = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                int go = Integer.parseInt(st.nextToken());
+                origin[i][j] = go;
+            }
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (origin[i][k] == 1 && origin[k][j] == 1) {
+                        origin[i][j] = 1;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(origin[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
